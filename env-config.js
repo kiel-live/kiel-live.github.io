659db3ac89d8f960dd2992f591972caf576411b4
anbraten <anton@ju60.de>
2022-05-26 11:50:24 +0000

    Deploying to master from  @ e63f6d5426576e59fb3b9fa977d34e086d714f06 🚀

--- env-config.js
+++
@@ -1,8 +0,0 @@
-window._env_ = {
-  APP_BACKEND_URL: 'https://kiel-live.ju60.de',
-  APP_MATOMO_SITE: '4',
-  APP_MATOMO_URL: '//stats.rzab.de',
-  APP_SENTRY_DSN: 'https://b309ec14770d40108ba24dd4019fed4c@error.rzab.de/7',
-  APP_TITLE: 'Kiel Live',
-  APP_TILE_SERVER_URL: '//tiles.slucky.de/styles/gray-matter/style.json',
-};

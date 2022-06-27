8911cadbcc682a6b1ff08283640c6b8a9d089cb6
lukashass <lukas@slucky.de>
2022-06-27 21:23:29 +0000

    Deploying to master from  @ f242aba27e941d0e0d826fc74efb4b7358b1f3b6 🚀

---
+++ assets/SettingsContainer.b17779c0.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./index.6561e59b.js";import{o as r,c as t,l as a}from"./vendor.3aab7ad4.js";const s={},n={class:"flex flex-col min-h-0 flex-grow overflow-y-auto p-2 md:max-w-200 md:mx-auto"};function c(e,l){return r(),t("div",n,[a(e.$slots,"default")])}var m=o(s,[["render",c]]);export{m as S};
+//# sourceMappingURL=SettingsContainer.b17779c0.js.map

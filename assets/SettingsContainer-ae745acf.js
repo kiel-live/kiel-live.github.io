20e60fb48f1012b8b518dd6785b0c2de58feb700
lukashass <lukas@slucky.de>
2023-03-23 11:07:35 +0000

    Deploying to master from  @ d8b9fb801ca549827479609f84502ebd8c1d9951 🚀

---
+++ assets/SettingsContainer-ae745acf.js
@@ -0,0 +1,2 @@
+import{_ as o,o as t,a as s,f as n}from"./index-933db775.js";const r={},a={class:"flex flex-col min-h-0 flex-grow overflow-y-auto p-2 md:max-w-200 md:mx-auto"};function c(e,l){return t(),s("div",a,[n(e.$slots,"default")])}const _=o(r,[["render",c]]);export{_ as S};
+//# sourceMappingURL=SettingsContainer-ae745acf.js.map

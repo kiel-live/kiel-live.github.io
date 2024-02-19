9180efb98435b5bfa786f5ea315619d39eb853a4
anbraten <6918444+anbraten@users.noreply.github.com>
2024-02-19 10:13:43 +0000

    Deploying to master from  @ aa581f3d82d621c1d19b031861dc5341845b53b0 🚀

--- assets/SettingsContainer-Fif6ij50.js
+++
@@ -1,2 +0,0 @@
-import{_ as o,o as t,a as s,f as n}from"./index-32chzDs8.js";const r={},a={class:"flex flex-col min-h-0 flex-grow overflow-y-auto p-2 md:max-w-200 md:mx-auto"};function c(e,l){return t(),s("div",a,[n(e.$slots,"default")])}const _=o(r,[["render",c]]);export{_ as S};
-//# sourceMappingURL=SettingsContainer-Fif6ij50.js.map

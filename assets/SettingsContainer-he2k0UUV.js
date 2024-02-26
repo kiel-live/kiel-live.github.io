86548b4d1999b9112620e2bfac26840f33480d2a
anbraten <6918444+anbraten@users.noreply.github.com>
2024-02-26 14:42:33 +0000

    Deploying to master from  @ 167ab26e30836594061354a7814f9a4d95626849 🚀

---
+++ assets/SettingsContainer-he2k0UUV.js
@@ -0,0 +1,2 @@
+import{_ as o,o as t,a as s,f as n}from"./index-NmukR1H0.js";const r={},a={class:"flex flex-col min-h-0 flex-grow overflow-y-auto p-2 md:max-w-200 md:mx-auto"};function c(e,l){return t(),s("div",a,[n(e.$slots,"default")])}const _=o(r,[["render",c]]);export{_ as S};
+//# sourceMappingURL=SettingsContainer-he2k0UUV.js.map

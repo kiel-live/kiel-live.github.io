7502e2f99f232cfc20c27546589de4b81d3d47cc
lukashass <lukas@slucky.de>
2026-02-08 12:59:40 +0000

    Deploying to master from @ kiel-live/kiel-live@3b046875001b67d8a7cd17a3f3fb954eca85882d 🚀

--- assets/useTrack-DmYPFwPy.js
+++
@@ -1,2 +0,0 @@
-function a(){const{umami:c}=globalThis;async function n(t,r){c==null||c.track(t,r)}return{track:n}}export{a as u};
-//# sourceMappingURL=useTrack-DmYPFwPy.js.map
---
+++ assets/useTrack-U4hIV2-U.js
@@ -0,0 +1,2 @@
+function r(){const{umami:a}=globalThis;async function c(n,t){a?.track(n,t)}return{track:c}}export{r as u};
+//# sourceMappingURL=useTrack-U4hIV2-U.js.map

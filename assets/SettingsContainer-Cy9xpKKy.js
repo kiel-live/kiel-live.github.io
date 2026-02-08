ff7ba184a2571889d8c8e2dbcbcc16c301e29b31
lukashass <lukas@slucky.de>
2026-02-08 17:10:37 +0000

    Deploying to master from @ kiel-live/kiel-live@50ca043b35a41158a954f241f556c8fd9157bbbc 🚀

--- assets/SettingsContainer-Cy9xpKKy.js
+++
@@ -1,2 +0,0 @@
-import{_ as t,c as o,a as s,g as a}from"./index-D5O7NvRW.js";const n={},r={id:"settings-container",class:"mt-(--safe-area-top,0px) flex min-h-0 grow flex-col overflow-y-auto p-2 md:mx-auto md:max-w-200"};function c(e,i){return s(),o("div",r,[a(e.$slots,"default")])}const f=t(n,[["render",c]]);export{f as S};
-//# sourceMappingURL=SettingsContainer-Cy9xpKKy.js.map
